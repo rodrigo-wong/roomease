5dccaaf08df9c73a07bdd8142e797af128e6dd1a
rodrigo-wong <wongmacr@gmail.com>
2025-02-02 11:29:41 -0500

    Add relationships and scopes to Bookable model for availability, orders, payments, and filtering by type

--- app/Models/Bookable.php
+++ app/Models/Bookable.php
@@ -15,4 +15,53 @@ class Bookable extends Model
     protected $casts = [
         'bookable_type' => BookableType::class, // Casts bookable_type as Enum
     ];
+
+    /**
+     * Get the availability for the bookable.
+     */
+    public function availability()
+    {
+        return $this->hasMany(BookableAvailability::class);
+    }
+
+    /**
+     * Get the orders associated with the bookable.
+     */
+    public function orders()
+    {
+        return $this->hasMany(OrderBookable::class);
+    }
+
+    /**
+     * Get the payments associated with the bookable.
+     */
+    public function payments()
+    {
+        return $this->hasManyThrough(Payment::class, OrderBookable::class);
+    }
+
+    /**
+     * Get the contractor associated with the bookable.
+     */
+    public function scopeContractors($query)
+    {
+        return $query->where('bookable_type', BookableType::CONTRACTOR);
+    }
+
+    /**
+     * Get the room associated with the bookable.
+     */
+    public function scopeRooms($query)
+    {
+        return $query->where('bookable_type', BookableType::ROOM);
+    }
+
+
+    /**
+     * Get the product associated with the bookable.
+     */
+    public function scopeProducts($query)
+    {
+        return $query->where('bookable_type', BookableType::PRODUCT);
+    }
 }
