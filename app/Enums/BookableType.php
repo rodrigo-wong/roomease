bb5c319f8ae522be74a9e08945cd6bf65d939e3b
rodrigo-wong <wongmacr@gmail.com>
2025-02-02 12:10:38 -0500

    Add values method to BookableType enum for easier access to enum cases

--- app/Enums/BookableType.php
+++ app/Enums/BookableType.php
@@ -7,4 +7,13 @@ enum BookableType: string
     case CONTRACTOR = 'contractor';
     case ROOM = 'room';
     case PRODUCT = 'product';
+
+    public static function values(): array
+    {
+        return [
+            self::CONTRACTOR,
+            self::ROOM,
+            self::PRODUCT,
+        ];
+    }
 }
