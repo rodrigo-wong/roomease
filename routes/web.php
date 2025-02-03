c24ad99e4664265142b4d9ebcf63f2e089f9892e
rodrigo-wong <wongmacr@gmail.com>
2025-02-03 18:39:25 -0500

    Update home route to render Booking component and add routes for available time slots and bookables

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,15 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\BookableController;
+use App\Models\Bookable;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+    return Inertia::render('Booking', [
+        'rooms' => Bookable::rooms()->get(),
     ]);
 });
-
+Route::get('/time-slots/{room}', [BookableController::class, 'getAvailableTimes'])->name('bookable.time-slots');
+Route::get('/available/bookables', [BookableController::class, 'getAvailableBookables'])->name('bookable.available');
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
