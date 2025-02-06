3cbd4c09c29da7c0734064001c24b19fe10aaf6a
rodrigo-wong <wongmacr@gmail.com>
2025-02-06 00:13:19 -0500

    Add bookable_id to fillable attributes in Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['product_category_id', 'serial_number', 'brand', 'name', 'description'];
+    protected $fillable = ['product_category_id', 'bookable_id', 'serial_number', 'brand', 'name', 'description'];
 
     public function category(): BelongsTo
     {
