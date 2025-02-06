0671fdc3a88de2d14c85afb3633b8b230bcc98eb
rodrigo-wong <wongmacr@gmail.com>
2025-02-05 21:04:36 -0500

    Add route for storing product categories in ProductCategoryController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use Inertia\Inertia;
+use App\Models\Bookable;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\BookableController;
-use App\Models\Bookable;
+use App\Http\Controllers\ProductCategoryController;
 
 Route::get('/', function () {
     return Inertia::render('Booking', [
@@ -14,6 +15,8 @@ Route::get('/', function () {
 });
 Route::get('/time-slots/{room}', [BookableController::class, 'getAvailableTimes'])->name('bookable.time-slots');
 Route::get('/available/bookables', [BookableController::class, 'getAvailableBookables'])->name('bookable.available');
+Route::post('/product/category', [ProductCategoryController::class, 'store'])->name('product.category.store');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
