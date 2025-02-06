5b9d512ab990c79ddd32b3e6bbc9746c8d74ef3a
rodrigo-wong <wongmacr@gmail.com>
2025-02-06 00:13:16 -0500

    Disable automatic timestamps in ContractorRole model

--- app/Models/ContractorRole.php
+++ app/Models/ContractorRole.php
@@ -6,13 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContractorRole extends Model
 {
-    //
+    public $timestamps = false;
+
     protected $fillable = ['name', 'description', 'rate'];
 
     public function contractors()
     {
         return $this->hasMany(Contractor::class);
     }
-    public $timestamps = false; // Disable automatic timestamps
-
 }
