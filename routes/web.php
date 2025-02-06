a12a7f0de4bad4f6382b83e1a837623e1bfbdc5f
rodrigo-wong <wongmacr@gmail.com>
2025-02-05 23:47:49 -0500

    Add route for storing contractor roles and reorder product category route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\BookableController;
+use App\Http\Controllers\ContractorRoleController;
 use App\Http\Controllers\ProductCategoryController;
 
 Route::get('/', function () {
@@ -15,7 +16,7 @@ Route::get('/', function () {
 });
 Route::get('/time-slots/{room}', [BookableController::class, 'getAvailableTimes'])->name('bookable.time-slots');
 Route::get('/available/bookables', [BookableController::class, 'getAvailableBookables'])->name('bookable.available');
-Route::post('/product/category', [ProductCategoryController::class, 'store'])->name('product.category.store');
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -29,9 +30,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-    Route::resource('bookables', BookableController::class);
 
+    Route::resource('bookables', BookableController::class);
+    Route::post('/product/category', [ProductCategoryController::class, 'store'])->name('product.category.store');
+    Route::post('/contractor/role', [ContractorRoleController::class, 'store'])->name('contractor.role.store');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
