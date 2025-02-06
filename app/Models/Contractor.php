e23a694b185d1d0eccddc2bd16f2824d3bc47122
rodrigo-wong <wongmacr@gmail.com>
2025-02-05 23:46:50 -0500

    Refactor Contractor model to update fillable properties and establish role relationship

--- app/Models/Contractor.php
+++ app/Models/Contractor.php
@@ -2,18 +2,24 @@
 
 namespace App\Models;
 
+use App\Models\ContractorRole;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Contractor extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['bookable_id', 'email', 'phone_number', 'role'];
+    protected $fillable = ['bookable_id', 'name', 'email', 'phone_number', 'role_id'];
 
     public function bookable(): BelongsTo
     {
         return $this->belongsTo(Bookable::class);
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(ContractorRole::class, 'role_id');
     }
 }
