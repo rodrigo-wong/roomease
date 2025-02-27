312a4d7703cb067c683996fc436db90db5c732e4
rodrigo-wong <wongmacr@gmail.com>
2025-02-26 22:11:06 -0500

    Add ContractorController with confirmation logic for orders

---
+++ app/Http/Controllers/ContractorController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Enums\OrderStatus;
+use App\Models\Contractor;
+use Illuminate\Http\Request;
+use App\Models\OrderBookable;
+use App\Models\ContractorRole;
+use App\Enums\OrderBookableStatus;
+use Illuminate\Support\Facades\Log;
+
+class ContractorController extends Controller
+{
+    public function confirm(Request $request)
+    {
+        Log::info($request->all());
+        $validated = $request->validate([
+            'order_id' => 'required|exists:orders,id',
+            'contractor_id' => 'required|exists:contractors,id',
+            'role_id' => 'required|exists:contractor_roles,id'
+        ]);
+        
+        /**
+         * @var Order $order
+         */
+        $order = Order::findOrFail($request->order_id);
+        /*
+        * @var OrderBookable $orderBookableToBeFilled
+        */
+        $orderBookableToBeFilled = $order->orderBookables()->where('status', OrderStatus::PENDING)->where('bookable_type', ContractorRole::class)->where('bookable_id', $validated['role_id'])->first();;
+        if(!$orderBookableToBeFilled) {
+            dd("Job already taken");
+        } else {
+            $orderBookableToBeFilled->status = OrderBookableStatus::CONFIRMED;
+            $orderBookableToBeFilled->bookable_id = $validated['contractor_id'];
+            $orderBookableToBeFilled->bookable_type = Contractor::class;
+            $orderBookableToBeFilled->save();
+        }
+
+        // dd($order->isCompleted());
+        if($order->isCompleted()) {
+            $order->status = OrderStatus::COMPLETED;
+        }
+        $order->save();
+
+        dd("Contractor confirmed");
+
+        return redirect()->back();
+    }
+}
