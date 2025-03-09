987582e0b826b2ba1bcccbe0675df4cbe7af5510
rodrigo-wong <wongmacr@gmail.com>
2025-03-09 19:20:58 -0400

    Add CleanupProcessingOrders command for processing order cleanup

---
+++ app/Console/Commands/CleanupProcessingOrders.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CleanupProcessingOrders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:cleanup-processing-orders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
