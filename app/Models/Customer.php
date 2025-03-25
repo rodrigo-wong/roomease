e63075f6254935adc6bd24abbe3ec3b34d217718
rodrigo-wong <wongmacr@gmail.com>
2025-03-25 00:57:34 -0400

    Add payments relationship to Customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -16,6 +16,11 @@ class Customer extends Model
         return $this->hasMany(Order::class);
     }
 
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+
     public function getFullNameAttribute(): string
     {
         return "{$this->first_name} {$this->last_name}";
