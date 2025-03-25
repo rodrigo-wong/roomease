2c195f1445da0084fbbced81c090bab256576f9a
rodrigo-wong <wongmacr@gmail.com>
2025-03-25 00:58:02 -0400

    Add scheduled task to delete processing orders older than 15 minutes

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Models\Order;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    Order::where('created_at', '<', now()->subMinutes(15))
+         ->where('status', 'processing')
+         ->delete();
+})->everyFifteenSeconds();
