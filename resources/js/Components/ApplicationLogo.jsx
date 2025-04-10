e80ada0557f9a5b5618340ed30af0736dd97c83e
rodrigo-wong <wongmacr@gmail.com>
2025-04-10 03:50:31 -0400

    Update ApplicationLogo component to use new SVG logo

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -2,7 +2,7 @@ export default function ApplicationLogo(props) {
     return (
         <img 
         className="block h-4 w-auto fill-current text-gray-800"
-        src="/images/arbelos_logo.png" 
+        src="/images/logo-textv-studios.svg" 
         alt="Company Logo"
         {...props} 
     />
