4f0be8f943cefadd0f82ba1498de2ea75cb919e3
rodrigo-wong <wongmacr@gmail.com>
2025-04-10 03:50:46 -0400

    Simplify AuthenticatedLayout usage in BookablesEdit component

--- resources/js/Pages/Bookables/Edit.jsx
+++ resources/js/Pages/Bookables/Edit.jsx
@@ -115,13 +115,7 @@ const BookablesEdit = ({
     };
 
     return (
-        <AuthenticatedLayout
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Edit Bookable
-                </h2>
-            }
-        >
+        <AuthenticatedLayout>
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
